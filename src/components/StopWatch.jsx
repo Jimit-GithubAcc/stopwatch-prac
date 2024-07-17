317495c5a25b4dcc1f2b173440e456589b002b4e
Jimit-GithubAcc <jimitnshah177@gmail.com>
2024-07-17 17:31:10 +0530

    added start, pause, stop & reset timer functionalities

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -8,22 +8,63 @@ const StopWatch = () => {
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const timerRef = useRef(0);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (isTimerRunning) {
+      timerRef.current = setInterval(() => {
+        setTimer((prev) => prev + 1);
+      }, 10);
+    } else {
+      clearInterval(timerRef.current);
+    }
 
-  const startTimer = () => {
-    timerRef.current = setInterval(() => {
-      setTimer((prev) => prev + 1);
-    });
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, [isTimerRunning]);
+
+  const toggleStartTimer = () => {
+    setIsTimerRunning((prev) => !prev);
+  };
+
+  const stopTimer = () => {
+    setIsTimerRunning(false);
+    // To Reset the timer back to 0
+    setTimeout(() => {
+      setTimer(0);
+    }, 2000);
+  };
+
+  const resetTimer = () => {
+    setTimer(0);
+    setIsTimerRunning(false);
+  };
+
+  const displayFormattedTime = (time) => {
+    const minutes = Math.floor((time / (1000 * 60)) % 60);
+    const seconds = Math.floor((time / 1000) % 60);
+    const milliseconds = Math.floor((time % 1000) / 10);
+
+    const getMinutes = minutes.toString().padStart(2, "0");
+    const getSeconds = seconds.toString().padStart(2, "0");
+    const getMilliseconds = milliseconds.toString().padStart(2, "0");
+
+    return `${getMinutes}:${getSeconds}:${getMilliseconds}`;
   };
 
   return (
     <div className="container">
       <h1 className="stopwatch-title">Stop Watch Demo</h1>
-      <h2 className="stopwatch-timer">{timer}</h2>
+      <h2 className="stopwatch-timer">{displayFormattedTime(timer)}</h2>
       <section className="btn-group">
-        <button className="start-btn">Start</button>
-        <button className="stop-btn">Stop</button>
-        <button className="reset-btn">Reset</button>
+        <button className="start-btn" onClick={toggleStartTimer}>
+          {isTimerRunning ? "Pause" : "Start"}
+        </button>
+        <button className="stop-btn" onClick={stopTimer}>
+          Stop
+        </button>
+        <button className="reset-btn" onClick={resetTimer}>
+          Reset
+        </button>
       </section>
     </div>
   );
