a68be74b4a4fa4a87a889972c06a9745ebcbd008
Jimit-GithubAcc <jimitnshah177@gmail.com>
2024-07-17 16:15:53 +0530

    created basic styles for stopwatch

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
+import "./StopWatch.css";
 
 const StopWatch = () => {
   const [timer, setTimer] = useState(0);
@@ -9,10 +10,21 @@ const StopWatch = () => {
 
   useEffect(() => {}, []);
 
+  const startTimer = () => {
+    timerRef.current = setInterval(() => {
+      setTimer((prev) => prev + 1);
+    });
+  };
+
   return (
-    <div>
-      StopWatch Compo
-      <h1>{timer}</h1>
+    <div className="container">
+      <h1 className="stopwatch-title">Stop Watch Demo</h1>
+      <h2 className="stopwatch-timer">{timer}</h2>
+      <section className="btn-group">
+        <button className="start-btn">Start</button>
+        <button className="stop-btn">Stop</button>
+        <button className="reset-btn">Reset</button>
+      </section>
     </div>
   );
 };
